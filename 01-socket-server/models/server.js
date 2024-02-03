384c940bc0b95ffc5cfd925edab9b329826ca32f
rodrigo lopez <rodrigo_l321@hotmail.com>
2024-02-03 16:40:59 -0600

    Primer comit

---
+++ 01-socket-server/models/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+const cors = require('cors');
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.server = http.createServer(this.app);
+        this.io = socketio(this.server);
+    }
+
+    configurarSockets() {
+        new Sockets(this.io);
+    }
+
+    middleware() {
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
+        this.app.use(cors());
+    }
+
+    execute() {
+        this.middleware();
+        this.configurarSockets();
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en puerto:', this.port);
+        })
+    }
+}
+
+module.exports = Server
